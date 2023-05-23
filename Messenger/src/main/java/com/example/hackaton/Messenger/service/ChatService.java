8d8e579d7cc91e13ef764c742a6232be8c0fa454
Katik0o <104070213+Katik0o@users.noreply.github.com>
2023-05-23 16:59:08 +0400

    upd dependencies

---
+++ Messenger/src/main/java/com/example/hackaton/Messenger/service/ChatService.java
@@ -0,0 +1,4 @@
+package com.example.hackaton.Messenger.service;
+
+public class ChatService {
+}
--- Messenger/src/main/java/com/example/hackaton/Messenger/spring/UserDetailsServiceImpl.java
+++ Messenger/src/main/java/com/example/hackaton/Messenger/spring/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.hackaton.Messenger.spring;
 
+import com.example.hackaton.Messenger.entity.User;
+import com.example.hackaton.Messenger.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
