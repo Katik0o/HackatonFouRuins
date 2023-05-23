578438066bd0421e751df901778ebbdfab1c8553
Kuznetsov-Dmitrii <dimanshik66@mail.ru>
2023-05-23 15:53:49 +0400

    Добавление ошибок

--- Messenger/src/main/java/com/example/hackaton/Messenger/web/controller/UserController.java
+++ Messenger/src/main/java/com/example/hackaton/Messenger/web/controller/UserController.java
@@ -1,11 +1,79 @@
 package com.example.hackaton.Messenger.web.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hackaton.Messenger.spring.JwtResponse;
+import com.example.hackaton.Messenger.spring.JwtUtils;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 
 @RequestMapping("/auth")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    MailClient mailClient;
+
+    @Autowired
+    AuthenticzationManager authenticationManager;
+
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+
+
+    private static UserForm userFormSt;
+    private static String confirmCodeSt;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail()
+        ));
+    }
+
+
+    @PostMapping("/registration")
+    public ResponseEntity<?> userRegistrationSubmit(@Valid @RequestBody UserForm userForm){
+        confirmCodeSt = UUID.randomUUID().toString();
+        userFormSt=userForm;
+        mailClient.sendMail( userForm.getEmail(),"Email confirm", confirmCodeSt);
+        return ResponseEntity.ok("true");
+    }
+
+
+    @PostMapping("/accept")
+    public ResponseEntity<?> userRegistrationAccept(){
+
+        return ResponseEntity.ok(UserModel.toModel(userService.save(userFormSt)));
+
+
+    }
+
+    @GetMapping("/accept")
+    public ResponseEntity<?> userRegistrationGetCode(){
+
+        return ResponseEntity.ok(confirmCodeSt);
+
+    }
 
 
 }
